463f6a1c09c6f805e4a8531755d6bfc01a594814
Alyona Golz <golz.alyona@gmail.com>
2024-03-26 21:47:51 +0400

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -2,6 +2,8 @@
 
 while true; do
   ARCH=$(uname -m)
+  DISTRIBUTION=$(cat /etc/redhat-release | awk '{print $1}')
+  VERSION=$(cat /etc/redhat-release | awk '{print $4}')
   KERNEL=$(uname -r)
   CPU_PHYSICAL=$(lscpu | grep "Socket(s):" | awk '{print $2}')
   VCPU=$(lscpu | grep "^CPU(s):" | awk '{print $2}')
@@ -9,15 +11,14 @@ while true; do
   DISK_USAGE=$(df -BG --output=size,used,pcent / | awk 'NR==2 {printf "%s/%s (%s)\n", $2, $1, $3}')
   CPU_LOAD=$(top -b -n1 | grep "Cpu(s)" | awk '{printf "%.1f%%", $2+$4}')
   LAST_BOOT=$(who -b | awk '{print $3, $4}')
-  LVM_STATUS=$(lvs --noheadings -o lv_active 2>/dev/null | grep -q "active" && echo "yes" || echo "no")
+  LVM_STATUS=$(lvs --noheadings -o lv_active LVMGroup 2>/dev/null | head -n 1 | grep -q "active" && echo "yes" || echo "no")
   TCP_CONNECTIONS=$(ss -tn state established '( dport = :ssh or sport = :ssh )' | grep -c -v LISTEN)
   USER_LOG=$(users | wc -w)
   IP_MAC=$(ip -4 -o addr show | awk '!/^[0-9]*: ?lo/ {print $4 " ("$6")"}')
   SUDO_CMDS=$(journalctl _COMM=sudo | grep COMMAND | wc -l)
 
   MESSAGE="
-#Architecture: $ARCH
-#Kernel Version: $KERNEL
+#Architecture: $DISTRIBUTION $KERNEL $VERSION $ARCH GNU/Linux
 #CPU physical : $CPU_PHYSICAL
 #vCPU : $VCPU
 #Memory Usage: $RAM_USAGE
