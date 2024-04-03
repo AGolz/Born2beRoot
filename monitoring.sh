bdf88a2f92c1beceaf689ed3ff459e51a7a7477f
Alyona Golz <golz.alyona@gmail.com>
2024-04-03 22:31:26 +0400

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
 while true; do
   ARCH=$(uname -m)
   DISTRIBUTION=$(cat /etc/redhat-release | awk '{print $1}')
@@ -11,7 +13,7 @@ while true; do
   DISK_USAGE=$(df -BG --output=size,used,pcent / | awk 'NR==2 {printf "%s/%s (%s)\n", $2, $1, $3}')
   CPU_LOAD=$(top -b -n1 | grep "Cpu(s)" | awk '{printf "%.1f%%", $2+$4}')
   LAST_BOOT=$(who -b | awk '{print $3, $4}')
-  LVM_STATUS=$(lvs --noheadings -o lv_active LVMGroup 2>/dev/null | head -n 1 | grep -q "active" && echo "yes" || echo "no")
+  LVM_STATUS=$(/usr/sbin/lvs --noheadings -o lv_active LVMGroup 2>/dev/null | head -n 1 | grep -q "active" && echo "yes" || echo "no")
   TCP_CONNECTIONS=$(ss -tn state established '( dport = :ssh or sport = :ssh )' | grep -c -v LISTEN)
   USER_LOG=$(users | wc -w)
   IP_MAC=$(ip -4 -o addr show | awk '!/^[0-9]*: ?lo/ {print $4 " ("$6")"}')
